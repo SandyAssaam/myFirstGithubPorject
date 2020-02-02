ef7ea3c6befa47c977bad4c38b7e4c64603a8a21
SandyAssaam <60569380+SandyAssaam@users.noreply.github.com>
2020-02-02 07:03:44 -0800

    Add files via upload

---
+++ sandy.py
@@ -0,0 +1,6 @@
+name = input('what is your name:?')
+age =input('how old are you:?')
+city = input('where do you live exaactly:?')
+print('welcome',name,'in jordan:')
+profession = input('what is your business:?')
+print('name:',name,'age:',age,'city:',city,'profession:',profession)
